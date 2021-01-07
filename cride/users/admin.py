ca1130b6986c7fd6389b71ee6d1aaf991bce795a
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-06 20:00:17 -0500

    Complete user's profile admin view

--- cride/users/admin.py
+++ cride/users/admin.py
@@ -7,17 +7,42 @@ from django.contrib.auth.admin import UserAdmin
 # Models
 from cride.users.models import User, Profile
 
-class CustomUserAdmin(UserAdmin):
-  """User model admin."""
-  list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_client')
-  list_filter = ('is_client', 'is_staff', 'created', 'modified')
-
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
   """Profile model admin."""
-
   list_display = ('user', 'reputation', 'rides_taken', 'rides_offered')
   search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
   list_filter = ('reputation',)
+
+  fieldsets = (
+    ('Profile', {
+      'fields': (
+        ('user', 'picture'),
+        ('biography')
+      )
+    }),
+    ('Stats', {
+      'fields': (
+        ('reputation'),
+        ('rides_taken', 'rides_offered'),
+      )
+    }),
+    ('Metadata', {
+      'fields': (('created', 'modified'),),
+    })
+  )
+
+  readonly_fields = ('created', 'modified')
+
+class ProfileInLine(admin.StackedInline):
+  model = Profile
+  can_delete = False
+  verbose_name_plural = 'profiles'
+
+class CustomUserAdmin(UserAdmin):
+  """User model admin."""
+  inlines = (ProfileInLine,)
+  list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_client')
+  list_filter = ('is_client', 'is_staff', 'created', 'modified')
 
 admin.site.register(User, CustomUserAdmin)
