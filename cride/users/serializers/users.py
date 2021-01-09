83ebae83259bba6b3e8e7b2d43ff7172db8aa9c5
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-09 15:59:41 -0500

    Verify account using JWT

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -131,3 +131,30 @@ class UserSignUpSerializer(serializers.Serializer):
     }
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
     return token
+
+class AccountVerificationSerializer(serializers.Serializer):
+  """Account verification serializer."""
+
+  token = serializers.CharField()
+
+  def validate_token(self, data):
+    """Verify token is valid."""
+    try:
+      payload = jwt.decode(data, settings.SECRET_KEY, algorithms=['HS256'])
+    except jwt.ExpiredSignature:
+      raise serializers.ValidationError('Verification link has expired.')
+    except jwt.PyJWTError:
+      raise serializers.ValidationError('Invalid token')
+
+    if payload['type'] != 'email_confirmation':
+      raise serializers.ValidationError('Invalid token')
+
+    self.context['payload'] = payload
+    return data
+
+  def save(self):
+    """Update user\'s verified status"""
+    payload = self.context['payload']
+    user = User.objects.get(username=payload['user'])
+    user.is_verified = True
+    user.save()
