69caf7c3e8fcdda531f779c827d8859cafce6dd9
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-10 12:09:39 -0500

    Creation of circles and validate limited circle

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -22,3 +22,16 @@ class CircleViewSet(viewsets.ModelViewSet):
     if self.action == 'list':
       return queryset.filter(is_public=True)
     return queryset
+
+  def perform_create(self, serializer):
+    """Assign circle admin."""
+    circle = serializer.save()
+    user = self.request.user
+    profile = user.profile
+    Membership.objects.create(
+      user=user,
+      profile=profile,
+      circle=circle,
+      is_admin=True,
+      remaining_invitations=10
+    )
