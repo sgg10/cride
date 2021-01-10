be980a3e89ae3ba4582504c28b4c366c9f67556c
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-10 09:51:19 -0500

    Update circle model (Membership)

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -18,6 +18,12 @@ class Circle(CRideModel):
   about = models.CharField('circle description', max_length=255)
   picture = models.ImageField(upload_to='circles/pictures', blank=True, null=True)
 
+  members = models.ManyToManyField(
+    'users.User',
+    through='circles.Membership',
+    through_fields=('circle', 'user')
+  )
+
   # Stats
   rides_offered = models.PositiveIntegerField(default=0)
   rides_taken = models.PositiveIntegerField(default=0)
