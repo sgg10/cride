7e1df04ea307b33b36a0e5cf8a2e2ebc19e266a7
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-11 15:44:14 -0500

    Implement JWT authentication
    Project has a JWT with email-verification and Django token authentication but now, change django to simple-jwt

--- cride/users/urls.py
+++ cride/users/urls.py
@@ -3,6 +3,12 @@
 # Django
 from django.urls import path
 
+# DRF Simple JWT
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView,
+  TokenRefreshView,
+)
+
 # Views
 from cride.users.views import (
   UserLoginAPIView,
@@ -14,4 +20,6 @@ urlpatterns = [
   path('users/login/', UserLoginAPIView.as_view(), name='login'),
   path('users/signup/', UserSignUpAPIView.as_view(), name='signup'),
   path('users/verify/', AccountVerificationAPIView.as_view(), name='verify'),
+  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
