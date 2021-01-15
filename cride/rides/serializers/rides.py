2f70912a7e2d9eaeb47c14fd38ffc2a29661e5a5
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-01-14 20:43:01 -0500

    Field validation of a serializer

--- cride/rides/serializers/rides.py
+++ cride/rides/serializers/rides.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 
 # Models
 from cride.rides.models import Ride
+from cride.circles.models import Membership
 
 # Utilities
 from datetime import timedelta
@@ -30,3 +31,50 @@ class CreateRideSerializer(serializers.ModelSerializer):
         'Departure time must be at least pass the next 20 minutes window.'
       )
     return data
+
+  def validate(self, data):
+    """Validate.
+
+      Verify that the person who offers the ride is member
+      and also the same user making the request.
+    """
+
+    if self.context['request'].user != data['offered_by']:
+      raise serializers.ValidationError('Rides offered on behalf of others are not allowed.')
+
+    user = data['offered_by']
+    circle = self.context['circle']
+    try:
+      membership = Membership.objects.get(
+        user=user, 
+        circle=circle, 
+        is_active=True)
+    except Membership.DoesNotExist:
+      raise serializers.ValidationError('User is not an active member of the circle.')
+
+    if data['arrival_date'] <= data['departure_date']:
+      raise serializers.ValidationError('Departure date must happen after arrival date.')
+
+    self.context['membership'] = membership
+    return data
+
+  def create(self, data):
+    """Create Ride and update stats."""
+    circle = self.context['circle']
+    ride = Ride.objects.create(**data, offered_in=circle)
+
+    # Circle
+    circle.rides_offered += 1
+    circle.save()
+
+    # Membership
+    membership = self.context['membership']
+    membership.rides_offered += 1
+    circle.save()
+
+    # Profile
+    profile = data['offered_by'].profile
+    profile.rides_offered += 1
+    profile.save()
+
+    return ride
